bb533238c5250294208c7fec6720d1fa30751ac3
Jake Gadaleta <me@jakegads.dev>
2021-12-01 16:36:02 -0500

    finished the first question for day 1

---
+++ 2021/day1/Day1/Part1.cs
@@ -0,0 +1,18 @@
+﻿using (StreamReader file = new StreamReader("C:\\Users\\Jake Gadaleta\\source\\repos\\Advent-Of-Code\\2021\\day1\\Day1\\input.txt"))
+{
+    int counter = -1;
+    int current = 0;
+    string ln;
+
+    while ((ln = file.ReadLine()) != null)
+    {
+        if(current < int.Parse(ln))
+        {
+            counter++;
+        }
+
+        current = int.Parse(ln);
+    }
+    file.Close();
+    Console.WriteLine(counter);
+}
---
+++ 2021/day1/Day1/Part2.cs
@@ -0,0 +1,3 @@
+﻿string[] lines = File.ReadAllLines(new("C:\\Users\\Jake Gadaleta\\source\\repos\\Advent-Of-Code\\2021\\day1\\Day1\\input.txt"));
+
+Console.WriteLine(lines);
