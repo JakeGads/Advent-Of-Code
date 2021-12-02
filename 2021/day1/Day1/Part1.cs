c23efab40fe01b306a833120930df0cd8f66a0a3
Jake Gadaleta <jake.gads@gmail.com>
2021-12-01 19:28:22 -0500

    syncing to run on desktop

--- 2021/day1/Day1/Part1.cs
+++ 2021/day1/Day1/Part1.cs
@@ -1,18 +1,62 @@
-﻿using (StreamReader file = new StreamReader("C:\\Users\\Jake Gadaleta\\source\\repos\\Advent-Of-Code\\2021\\day1\\Day1\\input.txt"))
-{
-    int counter = -1;
-    int current = 0;
-    string ln;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-    while ((ln = file.ReadLine()) != null)
+public static class Runner
+{
+    public static void Main(string[] args)
     {
-        if(current < int.Parse(ln))
+        string textFile = "C:\\Users\\Jake Gadaleta\\source\\repos\\Advent-Of-Code\\2021\\day1\\Day1\\input.txt";
+        two(textFile);
+    }
+
+    public static void one(string text)
+    {
+        using (StreamReader file = new StreamReader(text))
         {
-            counter++;
+            int counter = -1;
+            int current = 0;
+            string ln;
+
+            while ((ln = file.ReadLine()) != null)
+            {
+                if (current < int.Parse(ln))
+                {
+                    counter++;
+                }
+
+                current = int.Parse(ln);
+            }
+            file.Close();
+            Console.WriteLine(counter);
+        }
+    }
+
+    public static void two(string text)
+    {
+        List<int> values = new List<int>();
+        int count = 0;
+        using (StreamReader file = new StreamReader(text))
+        {
+            string ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                values.Add(int.Parse(ln));
+            }
         }
 
-        current = int.Parse(ln);
+        for(int i = 0; i + 3 < values.Count; i++)
+        {
+            int a = values[i] + values[i+1] + values[i+2];
+            int b = values[i + 3] + values[i + 1] + values[i + 2];
+
+            if(b < a)
+            {
+                count += 1;
+            }
+        }
+
+        Console.WriteLine(count);
+
     }
-    file.Close();
-    Console.WriteLine(counter);
 }
--- 2021/day1/Day1/Part2.cs
+++
@@ -1,3 +0,0 @@
-﻿string[] lines = File.ReadAllLines(new("C:\\Users\\Jake Gadaleta\\source\\repos\\Advent-Of-Code\\2021\\day1\\Day1\\input.txt"));
-
-Console.WriteLine(lines);
