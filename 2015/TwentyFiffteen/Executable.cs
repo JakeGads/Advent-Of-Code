eb2b3a2833a47bb5a2342cddf663b33afb8a579a
Jake Gadaleta <jgadaleta@ceiamerica.com>
2021-12-03 15:12:30 -0500

    finished 2015 day 2

--- 2015/TwentyFiffteen/Executable.cs
+++ 2015/TwentyFiffteen/Executable.cs
@@ -4,6 +4,6 @@ public class Executable
 {
 	public static void Main(String[] args)
     {
-        OneTwo.Program();
+        TwoTwo.Program();
     }
 }
---
+++ 2015/TwentyFiffteen/TwoOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TwoOne
+{
+    private static string filepath = @"Data\two.txt";
+    public static void Program()
+    {
+        using (StreamReader sr = new StreamReader(filepath))
+        {
+            int total = 0;
+            string line;
+            while((line = sr.ReadLine()) != null)
+            {
+                // l w h
+                int[] a = Array.ConvertAll(line.Split('x'), s => int.Parse(s));
+                Array.Sort(a);
+                // 2*l*w + 2*w*h + 2*h*l + slack
+                // slack is shortest + next
+                total += ((2 * a[0] * a[1]) + (2 * a[0] * a[2]) + (2 * a[1] * a[2])) + (a[0] * a[1]);
+            }
+            Console.WriteLine(total);
+        }
+
+    }
+}
+
